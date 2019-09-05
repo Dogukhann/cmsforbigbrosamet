1a83643d3a9f216c71a774867aa69c1be4b3c538
Dogukhann <doguturk1062@gmail.com>
2019-09-05 18:00:00 +0300

    Add files via upload

---
+++ contr.js
@@ -0,0 +1,18 @@
+var x = document.querySelector('.get');
+var z = document.querySelector('.minCont');
+var y = document.getElementById('#title');
+var e = document.querySelector('#big');
+var d = document.querySelector('#titlemis');
+var minimumInp = document.querySelector('#minInput');
+x.addEventListener('click',myFunction);
+
+
+/* Dom manipulation, To-do list*/
+
+/*function myFunction(){
+    var zzz = e.innerHTML += z.value +'<br>';
+    var baslik = d.innerHTML += minimumInp.value + '<br>';
+    console.log(zzz);
+    console.log(baslik);
+}
+*/
